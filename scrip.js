c94030f75a45bc922e3f14bcdd99e9dba3539f06
KilFex <elivan1251996@gmail.com>
2024-03-13 23:43:01 -0500

    falta_actualizar

--- scrip.js
+++ scrip.js
@@ -1,14 +1,38 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var audio = document.getElementById('audio'); 
+document.addEventListener('DOMContentLoaded', function () {
+    const audio = document.getElementById('audio');
+    const playPauseButton = document.getElementById('playPausa');
+    const nextButton = document.getElementById('siguiente');
+    const prevButton = document.getElementById('anterior');
     
-    function reproducirMusica() {
-        if (audio.paused) {
-            audio.play(); 
-        } else {
-            audio.pause(); 
-        }
+    let currentTrack = 0;
+    const tracks = document.querySelectorAll('source');
+  
+    function playPause() {
+      if (audio.paused) {
+        audio.play();
+        playPauseButton.innerHTML = '<img src="./icons/Pause_fill.svg" />';
+      } else {
+        audio.pause();
+        playPauseButton.innerHTML = '<img src="./icons/Play_fill.svg" />';
+      }
     }
-
-    document.getElementById('play').addEventListener('click', reproducirMusica);
-    
-});
+  
+    function nextTrack() {
+      currentTrack = (currentTrack + 1) % tracks.length;
+      audio.src = tracks[currentTrack].src;
+      audio.play();
+      playPauseButton.innerHTML = '<img src="./icons/Pause_fill.svg" />';
+    }
+  
+    function prevTrack() {
+      currentTrack = (currentTrack - 1 + tracks.length) % tracks.length;
+      audio.src = tracks[currentTrack].src;
+      audio.play();
+      playPauseButton.innerHTML = '<img src="./icons/Pause_fill.svg" />';
+    }
+  
+    playPauseButton.addEventListener('click', playPause);
+    nextButton.addEventListener('click', nextTrack);
+    prevButton.addEventListener('click', prevTrack);
+  });
+  
